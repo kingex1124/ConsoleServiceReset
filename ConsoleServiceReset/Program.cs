a653aec01a08d89d9b312b7506544223563088e3
kevan.chen <kevan.chen@sinopa.com>
2020-05-29 15:10:06 +0800

    重啟服務，web服務，都增加retry功能 等候時間調整

--- ConsoleServiceReset/Program.cs
+++ ConsoleServiceReset/Program.cs
@@ -14,8 +14,12 @@ namespace ConsoleServiceReset
     {
         static void Main(string[] args)
         {
-            Program.RestartWebSiteService("WebTest", 10);
-            //Program.RestartService("ATest", 10000);
+            Program.RestartWebSiteService("WebTest", 2000);
+
+            //Program.RestartService("StockGateway", 15000);
+            Program.RestartService("ATest", 15000);
+            Console.WriteLine("end");
+            Console.ReadLine();
         }
 
         /// <summary>
@@ -26,27 +30,38 @@ namespace ConsoleServiceReset
         public static void RestartWebSiteService(string webSiteName,int timeoutMilliseconds)
         {
             var server = new ServerManager();
-
             try
             {
                 int millisec1 = Environment.TickCount;
                 TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
+    
+                var site = server.Sites.FirstOrDefault(s => s.Name == webSiteName);
 
-                var site = server.Sites.FirstOrDefault(s => s.Name == "webSiteName");
+                // 取得站台目前狀態
+                Console.WriteLine(site.State);
                 if (site != null)
-                {
+                {                 
                     //stop the site
                     site.Stop();
 
-                    Thread.Sleep(timeout);
+                    // 取得停止站台後的狀態
+                    Console.WriteLine(site.State);
 
-                    //start the site
-                    site.Start();
+                    while (site.State.ToString() == "Stopped")
+                    {
+                        //等待一段時間
+                        Thread.Sleep(timeout);
+
+                        //start the site
+                        site.Start();
+                    }
+                    // 取得啟動站台後的狀態
+                    Console.WriteLine(site.State);
                 }
             }
             catch (Exception ex)
             {
-
+                Console.WriteLine(ex.Message);
             }
         }
 
@@ -61,22 +76,38 @@ namespace ConsoleServiceReset
             ServiceController service = new ServiceController(serviceName);
             try
             {
-                int millisec1 = Environment.TickCount;
                 TimeSpan timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds);
 
-                service.Stop();
-                service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+                //取得服務目前狀態
+                Console.WriteLine(service.Status);
 
-                // count the rest of the timeout
-                int millisec2 = Environment.TickCount;
-                timeout = TimeSpan.FromMilliseconds(timeoutMilliseconds - (millisec2 - millisec1));
+                //啟動中就關閉重啟，如果是關閉中，就直接啟動
+                if (service.Status.ToString() == "Started")
+                {
+                    service.Stop();
+                    //執行停止
+                    // 設置timeout表示等候關閉 或是啟動的時間，不設定表示無止境等待
+                    //service.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
 
-                service.Start();
-                service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                    //取得服務停止後的狀態
+                    Console.WriteLine("停止後:" + service.Status);
+                }
+                // retry
+                while (service.Status.ToString() == "Stopped")
+                {
+                    service.Start();
+                    //執行啟動
+                    //service.WaitForStatus(ServiceControllerStatus.Running, timeout);
+                    service.WaitForStatus(ServiceControllerStatus.Running);
+                }
+
+                //取得服務重啟後的狀態
+                Console.WriteLine("重啟後:" + service.Status);
             }
             catch (Exception ex)
             {
-                // ...
+                Console.WriteLine(ex.Message);
             }   
         }
     }
